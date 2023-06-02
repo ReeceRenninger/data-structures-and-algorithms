66842a0a37125835cbb8e09fddfab1c69088ab5a
ReeceRenninger <renningerreece@gmail.com>
2023-06-02 12:26:32 -0700

    set up for trees challenges

---
+++ javascript/trees/index.js
@@ -0,0 +1,115 @@
+'use strict';
+
+
+class Node {
+  constructor(value) {
+    this.value = value;
+    this.left = null;
+    this.right = null;
+  }
+
+}
+
+//not required for code Challenge 15 but useful in future
+class KaryNode {
+  constructor(value, k) {
+    this.value = value;
+    this.children = new Array(k).fill(null);
+  }
+}
+
+//not required for code Challenge 15 but useful in future
+class KaryTree {
+  constructor() {
+    this.root = null;
+  }
+}
+
+class Tree {
+  constructor() {
+    this.root = null;
+  }
+
+  preOrder() {
+    const results = [];
+    const traverse = (node) => {
+      //base case (do the thing)
+      results.push(node.value);
+
+      //left and right recursive cases
+      if (node.left) {
+        traverse(node.left);
+      }
+      if (node.right) {
+        traverse(node.right);
+      }
+      // note IF there is NO LEFT and NO RIGHT then function is done.
+    };
+    // ** invoke our helper function
+    traverse(this.root);
+    return results;
+  }
+
+  inOrder() {
+    const results = [];
+    const traverse = (node) => {
+
+      if (node.left) {
+        traverse(node.left);
+      }
+      results.push(node.value); //performing our base case here makes this inOrder
+
+      if (node.right) {
+        traverse(node.right);
+      }
+
+      // note IF there is NO LEFT and NO RIGHT then function is done.
+    };
+    // ** invoke our helper function
+    traverse(this.root);
+    return results;
+
+  }
+
+  postOrder() {
+    const results = [];
+    const traverse = (node) => {
+
+      if (node.left) {
+        traverse(node.left);
+      }
+
+      if (node.right) {
+        traverse(node.right);
+      }
+      results.push(node.value); //performing our base case here makes this postOrder
+    };
+    // ** invoke our helper function
+    traverse(this.root);
+    return results;
+  }
+
+} // end of Tree class
+
+// CREATING A TREE FROM WHITEBOARD
+let tree = new Tree();
+tree.root = new Node(10);
+tree.root.left = new Node(5);
+tree.root.left.left = new Node(1);
+tree.root.left.right = new Node(8);
+tree.root.right = new Node(15)
+tree.root.right.right = new Node(17);
+
+console.log('-----------------pre order---------');
+let results = tree.preOrder();
+console.log('preOrder results:', results);
+
+console.log('----------------in order---------');
+let results = tree.inOrder();
+console.log('inOrder results:', results);
+
+console.log('-----------------post order---------');
+let results = tree.postOrder();
+console.log('postOrder results:', results);
+
+module.exports = { Tree };
