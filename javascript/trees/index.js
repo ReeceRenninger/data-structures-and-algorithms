02357752082e5e6e42ceb705df363905a4ab33ae
ReeceRenninger <renningerreece@gmail.com>
2023-06-02 15:29:57 -0700

    trees set up

--- javascript/trees/index.js
+++ javascript/trees/index.js
@@ -1,16 +1,14 @@
 'use strict';
 
-
 class Node {
   constructor(value) {
     this.value = value;
     this.left = null;
     this.right = null;
   }
-
 }
 
-//not required for code Challenge 15 but useful in future
+// not required for Code challenge 15
 class KaryNode {
   constructor(value, k) {
     this.value = value;
@@ -18,7 +16,7 @@ class KaryNode {
   }
 }
 
-//not required for code Challenge 15 but useful in future
+// not required for Code challenge 15
 class KaryTree {
   constructor() {
     this.root = null;
@@ -32,84 +30,75 @@ class Tree {
 
   preOrder() {
     const results = [];
+
+    // defining helper our function
     const traverse = (node) => {
-      //base case (do the thing)
+      // base case (do the thing)
       results.push(node.value);
 
-      //left and right recursive cases
+      // left and right recursive cases
       if (node.left) {
         traverse(node.left);
       }
       if (node.right) {
         traverse(node.right);
       }
-      // note IF there is NO LEFT and NO RIGHT then function is done.
+      //IF there is no left and no right WE ARE DONE WITH THIS FUNCTION
     };
-    // ** invoke our helper function
+    // START THE RECURSIVE STUFF HERE
     traverse(this.root);
     return results;
   }
 
   inOrder() {
     const results = [];
+
     const traverse = (node) => {
 
       if (node.left) {
         traverse(node.left);
       }
-      results.push(node.value); //performing our base case here makes this inOrder
-
+      results.push(node.value); // performing our base case here makes this inOrder
       if (node.right) {
         traverse(node.right);
       }
-
-      // note IF there is NO LEFT and NO RIGHT then function is done.
     };
-    // ** invoke our helper function
     traverse(this.root);
     return results;
-
   }
 
   postOrder() {
     const results = [];
-    const traverse = (node) => {
 
+    const traverse = (node) => {
       if (node.left) {
         traverse(node.left);
       }
-
       if (node.right) {
         traverse(node.right);
       }
-      results.push(node.value); //performing our base case here makes this postOrder
+      results.push(node.value); // performing our base case here makes this postOrder
     };
-    // ** invoke our helper function
     traverse(this.root);
     return results;
   }
 
-} // end of Tree class
+}
 
-// CREATING A TREE FROM WHITEBOARD
 let tree = new Tree();
 tree.root = new Node(10);
 tree.root.left = new Node(5);
+tree.root.right = new Node(15);
 tree.root.left.left = new Node(1);
 tree.root.left.right = new Node(8);
-tree.root.right = new Node(15)
 tree.root.right.right = new Node(17);
 
-console.log('-----------------pre order---------');
+console.log('-------preOrder-------');
 let results = tree.preOrder();
 console.log('preOrder results:', results);
-
-console.log('----------------in order---------');
-let results = tree.inOrder();
+console.log('-------inOrder-------');
+results = tree.inOrder();
 console.log('inOrder results:', results);
-
-console.log('-----------------post order---------');
-let results = tree.postOrder();
+console.log('-------postOrder-------');
+results = tree.postOrder();
 console.log('postOrder results:', results);
-
-module.exports = { Tree };
